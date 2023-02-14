1bdf02994dcfb8c93172f9b862a4a6088876b79a
VyacheslavSay <viacheslav.saiapin@yandex.ru>
2023-02-14 19:30:46 +0300

    remove validation

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 const corsOptions = {
     origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
+    credentials: true,//access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
 
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -12,15 +12,15 @@ const {body} = require('express-validator')
 router.get('/products', ProductsController.getProducts)
 router.get('/additives', AdditivesController.getAdditives)
 router.post('/registration',
-    body('login').isLength({min: 10, max: 30}),
-    body('login').isString(),
-    body('password').isLength({min: 10, max: 30}),
-    body('password').isString()
-    ,UserController.registration)
+    // body('login').isLength({min: 10, max: 30}),
+    // body('login').isString(),
+    // body('password').isLength({min: 10, max: 30}),
+    // body('password').isString(),
+    UserController.registration)
 
 router.post('/login', UserController.login)
 router.get('/logout', UserController.logout)
 router.get('/refresh', UserController.refresh)
-router.get('/create', authMiddleware ,UserController.create)
+router.get('/createOrder', authMiddleware ,UserController.create)
 
 module.exports = router;
