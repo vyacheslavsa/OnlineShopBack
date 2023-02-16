6c448cc91142003f4c059ec8bdb60d3c94f09c49
Vyacheslav <viacheslav.saiapin@gmail.com>
2023-02-16 18:04:35 +0300

    add validation

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,17 +4,33 @@ const UserController = require('../controllers/User.controller')
 const ProductsController = require('../controllers/Products.controller')
 const AdditivesController = require('../controllers/Additives.controller')
 const authMiddleware = require('../../middlewares/auth-middleware')
-const {body} = require('express-validator')
+const {check} = require('express-validator')
+
+const validation = [
+
+    check('login')
+        .isAlphanumeric(['en-US'])
+        .withMessage('Поле логин должно состоять только из латинских букв и цифр')
+        .notEmpty()
+        .withMessage('Поле не может быть пустым')
+        .isLength({ min:6, max:30 })
+        .withMessage('Поле логина должно быть не менее 6 символов и не более 30'),
+
+    check('password')
+        .isAlphanumeric(['en-US'])
+        .withMessage('Поле пароль должно состоять только из латинских букв и цифр')
+        .notEmpty()
+        .withMessage('Поле пароля не может быть пустым')
+        .isLength({min:6,max:30})
+        .withMessage('Поле пароля должно быть не меньше 6 символов и не более 30')
+        .matches(/\d/)
+        .withMessage("Пароль должен содержать минимум 1 цифру")
+
+ ]
 
 router.get('/products', ProductsController.getProducts)
 router.get('/additives', AdditivesController.getAdditives)
-router.post('/registration',
-    body('login').isLength({min: 10, max: 30}),
-    body('login').isString(),
-    body('password').isLength({min: 10, max: 30}),
-    body('password').isString(),
-    UserController.registration)
-
+router.post('/registration',validation, UserController.registration)
 router.post('/login', UserController.login)
 router.get('/logout', UserController.logout)
 router.get('/refresh', UserController.refresh)
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,59 +1,57 @@
 const UserModel = require("../models/User");
 const bcrypt = require("bcrypt");
-const tokenService = require('./token-service')
-const UserDto = require('../dtos/user-dto')
-const ApiError = require('../exceptions/api-error')
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
+const ApiError = require("../exceptions/api-error");
 
 class UserService {
-    async registration(login, password) {
-        const candidate = await UserModel.findOne({login})
-        if (candidate) {
-            throw ApiError.BadRequest(`Логин ${login} уже существует`)
-        }
-        const hashPass = await bcrypt.hash(password, 1)
-        const user = await UserModel.create({login: login, passHash: hashPass});
-        const userDto = new UserDto(user)
-        const tokens = tokenService.generateToken({...userDto})
-        await tokenService.saveToken(userDto.id, tokens.refreshToken)
-        return {...tokens, user: userDto}
+  async registration(login, password) {
+    const candidate = await UserModel.findOne({ login });
+    if (candidate) {
+      throw ApiError.BadRequest(`Логин ${login} уже существует`);
+    }
+    const hashPass = await bcrypt.hash(password, 1);
+    const user = await UserModel.create({ login: login, passHash: hashPass });
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+
+  async login(login, password) {
+    const user = await UserModel.findOne({ login });
+    if (!user) {
+      throw ApiError.BadRequest("Пользователь с таким логином не найден");
+    }
+    const isPasswordEquals = await bcrypt.compare(password, user.passHash);
+    if (!isPasswordEquals) {
+      throw ApiError.BadRequest("Неверный пароль");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDB = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDB) {
+      throw ApiError.UnauthorizedError();
     }
 
-    async login(login, password){
-
-        const user = await UserModel.findOne({login})
-        if(!user) {
-            throw ApiError.BadRequest('Пользователь с таким логином не найден')
-        }
-        const isPasswordEquals = await bcrypt.compare(password, user.passHash)
-        if(!isPasswordEquals){
-            throw ApiError.BadRequest('Неверный пароль')
-        }
-        const userDto = new UserDto(user);
-        const tokens = tokenService.generateToken({...userDto})
-        await tokenService.saveToken(userDto.id, tokens.refreshToken)
-        return {...tokens, user: userDto}
-    }
-    async logout (refreshToken){
-        const token = await tokenService.removeToken(refreshToken)
-        return token;
-    }
-    async refresh (refreshToken){
-        if(!refreshToken){
-            throw ApiError.UnauthorizedError();
-        }
-        const userData = await tokenService.validateRefreshToken(refreshToken);
-        const tokenFromDB = await tokenService.findToken(refreshToken);
-        console.log(tokenFromDB, 'tokenFromDB')
-        if (!userData || !tokenFromDB){
-            throw ApiError.UnauthorizedError();
-        }
-
-        const user = await UserModel.findById(userData.id)
-        const userDto = new UserDto(user);
-        const tokens = tokenService.generateToken({...userDto})
-        await tokenService.saveToken(userDto.id, tokens.refreshToken)
-        return {...tokens, user: userDto}
-    }
+    const user = await UserModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
 }
 
-module.exports = new UserService()
+module.exports = new UserService();
